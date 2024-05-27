bf5e3a7159631efb3cc0e83df8aa92cdec8b289d
Chris Jeganathan <4092952+chrisjeg@users.noreply.github.com>
2024-05-27 19:24:09 +0100

    My first compute module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+import { ComputeModule } from "@chrisjeg/compute-module";
+import { Type } from "@sinclair/typebox";
+
+const module = new ComputeModule({
+  logger: console,
+  definitions: {
+    currentDate: {
+      input: Type.Object({
+        foo: Type.String(),
+      }),
+      output: Type.Object({
+        foo: Type.String(),
+        currentDate: Type.String(),
+      }),
+    },
+  },
+});
+
+module.register("currentDate", async (input) => {
+  return {
+    foo: input.foo,
+    currentDate: new Date().toISOString(),
+  };
+});
